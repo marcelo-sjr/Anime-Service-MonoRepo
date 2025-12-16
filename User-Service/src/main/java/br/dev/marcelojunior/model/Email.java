2629fa2a94c25f229fe635286844abcff17dd9d7
marcelo-sjr <marcelodasilvajr2@gmail.com>
2025-12-16 09:18:30 -0300

    Creation of user microservice along with User and Email entities

---
+++ User-Service/src/main/java/br/dev/marcelojunior/model/Email.java
@@ -0,0 +1,32 @@
+package br.dev.marcelojunior.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+@Embeddable
+@Getter
+public class Email {
+
+    @Column(nullable = false, unique = true,name = "user_email")
+    private String email;
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9]+([._%+-][A-Za-z0-9]+)*@([A-Za-z0-9]+(-[A-Za-z0-9]+)*\\.)+[A-Za-z]{2,}$";
+    private static final Pattern PATTERN = Pattern.compile(EMAIL_REGEX);
+
+
+    protected Email(){}
+
+    public Email(String email){
+        if (!isEmailValid(email)) {
+            throw new IllegalArgumentException("invalid email address: " + email);
+        }
+        this.email = email.toLowerCase(Locale.ROOT);
+    }
+
+    private boolean isEmailValid(String email){
+        return PATTERN.matcher(email).matches();
+    }
+}
---
+++ User-Service/src/main/java/br/dev/marcelojunior/model/Users.java
@@ -0,0 +1,24 @@
+package br.dev.marcelojunior.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Embedded
+    private Email email;
+}

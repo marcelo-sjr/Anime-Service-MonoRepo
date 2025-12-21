9f7f1804830ecfc351a3b00a5ff25fc80d0a3e7a
marcelo-sjr <marcelodasilvajr2@gmail.com>
2025-12-21 07:27:18 -0300

    DataIntegrityViolationException now treated by the GlobalExceptionHandler

--- Commons-Core/src/main/java/br/dev/marcelojunior/exceptions/GlobalExceptionHandler.java
+++ Commons-Core/src/main/java/br/dev/marcelojunior/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package br.dev.marcelojunior.exceptions;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,4 +18,12 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new GlobalExceptionMessage(e.getStatusCode(),e.getReason()), e.getStatusCode());
     }
 
+    @ExceptionHandler
+    public ResponseEntity<GlobalExceptionMessage> dataIntegrityViolationHandler(DataIntegrityViolationException e){
+        log.atError().setCause(e).setMessage(e.getMessage()).log();
+        return new ResponseEntity<>(new GlobalExceptionMessage(
+                HttpStatus.CONFLICT,
+                "Registry already exists or violates a database constraint"),
+                HttpStatus.CONFLICT);
+    }
 }
